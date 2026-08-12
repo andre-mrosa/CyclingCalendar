cbf46e5c5715a37bd173a5f2eb829bdbd64ada1d
André Rosa <andre.rosa1603@gmail.com>
2026-08-12 22:45:37 +0100

    fix: add missing useRef import for infinite scroll

--- app/components/CalendarView.js
+++ app/components/CalendarView.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useSettingsStore } from '../store/useSettingsStore';
 import useSWR from 'swr';
 import CustomSelect from './CustomSelect';
