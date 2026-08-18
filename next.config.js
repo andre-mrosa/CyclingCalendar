e130fc9e3d00f618c9936371bc52b561ccb1dff9
André Rosa <andre.rosa1603@gmail.com>
2026-08-18 13:18:18 +0100

    chore: add turbopack config to silence next16 build error

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const withPWA = withPWAInit({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  turbopack: {}
 };
 
 module.exports = withPWA(nextConfig);
