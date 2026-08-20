e4400dd33d2b3b283579e2fb6965892f888371a5
André Rosa <andre.rosa1603@gmail.com>
2026-08-20 10:23:22 +0100

    chore: clean up debug and scratch files from repository root

--- check.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-pool.query("SELECT title, insurance FROM \"Evento\" WHERE title LIKE '%Medio Tejo%' LIMIT 1")
-    .then(res => { console.log(res.rows[0]); pool.end(); })
-    .catch(err => { console.error(err); pool.end(); });
